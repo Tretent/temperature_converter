4019abe575ef9c2b828ef7ecb4b062190dd98466
Roberto Pasquali <pasquali.roberto@gmail.com>
2023-10-10 22:18:33 +0200

    add conversion logic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Temperature converter");
-    println!("Please enter a temperature in Fahrenheit:")
+    println!("Fahrenheit to Celsius converter");
+    println!("Please enter a temperature in Fahrenheit:");
 
+    let mut temperature_f = String::new();
 
+    io::stdin()
+        .read_line(&mut temperature_f)
+        .expect("Failed to read line");
+
+    let temperature_f = temperature_f.trim().parse::<i32>().unwrap();
+    let temperature_c = (temperature_f - 32) * 5 / 9;
+
+    println!("The temperature in Celsius is: {}", temperature_c)
 }
